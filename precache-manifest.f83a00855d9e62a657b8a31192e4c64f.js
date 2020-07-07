1fe40e05e58b34fe03986713a527b0652fb73f26
Paulo Ariel Pareja <paulo.pareja@hotmail.es>
2020-07-06 21:59:28 -0300

    Updates

---
+++ precache-manifest.f83a00855d9e62a657b8a31192e4c64f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962cd7d4863728e1189daf41a9afecb4",
+    "url": "/frontend-red-social-react/index.html"
+  },
+  {
+    "revision": "4e2016012a24f6dc2886",
+    "url": "/frontend-red-social-react/static/css/2.9f98d992.chunk.css"
+  },
+  {
+    "revision": "c1c603fe0e74aa11ad46",
+    "url": "/frontend-red-social-react/static/css/main.52e815fc.chunk.css"
+  },
+  {
+    "revision": "4e2016012a24f6dc2886",
+    "url": "/frontend-red-social-react/static/js/2.fab71675.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/frontend-red-social-react/static/js/2.fab71675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c603fe0e74aa11ad46",
+    "url": "/frontend-red-social-react/static/js/main.f1166a99.chunk.js"
+  },
+  {
+    "revision": "09457923a089ea3527ec",
+    "url": "/frontend-red-social-react/static/js/runtime-main.f0a79e1a.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/frontend-red-social-react/static/media/camara.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/frontend-red-social-react/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/frontend-red-social-react/static/media/date-birth.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/frontend-red-social-react/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/frontend-red-social-react/static/media/links.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/frontend-red-social-react/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/frontend-red-social-react/static/media/logo-white.41f65f75.png"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/frontend-red-social-react/static/media/logo.1769587d.png"
+  }
+]);
